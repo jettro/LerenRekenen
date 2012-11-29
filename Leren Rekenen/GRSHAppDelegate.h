1fe11b1d85aab7fbf72bf29d770050107714c2c1
Jettro Coenradie <jettro@gridshore.nl>
2012-11-29 14:05:41 +0100

    Initial Commit

---
+++ Leren Rekenen/GRSHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRSHAppDelegate.h
+//  Leren Rekenen
+//
+//  Created by Jettro Coenradie on 11/29/12.
+//  Copyright (c) 2012 Jettro Coenradie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRSHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Leren Rekenen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Leren Rekenen
+//
+//  Created by Jettro Coenradie on 11/29/12.
+//  Copyright (c) 2012 Jettro Coenradie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRSHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRSHAppDelegate class]));
+    }
+}
